4b345086b6102cc9a1c5684841effefdda84610d
Leonardo Bisaro <lbisaro@tanet.com.ar>
2018-09-15 10:23:57 -0300

    2

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
-phpinfo();
+
+session_start();
+if (!isset($_SESSION['count'])) {
+  $_SESSION['count'] = 0;
+} else {
+  $_SESSION['count']++;
+}
+
+echo "<pre>";
+var_dump($_SESSION);
+echo "</pre>";
 ?>
