57ed18dbe38702f4ea2341096ce78bbdda44f5fd
Leonardo Bisaro <lbisaro@tanet.com.ar>
2018-09-17 11:05:28 -0300

    3

--- index.php
+++ index.php
@@ -10,4 +10,12 @@ if (!isset($_SESSION['count'])) {
 echo "<pre>";
 var_dump($_SESSION);
 echo "</pre>";
+echo "<h2>_POST</h2>";
+echo "<pre>";
+var_dump($_POST);
+echo "</pre>";
+echo "<h2>_GET</h2>";
+echo "<pre>";
+var_dump($_GET);
+echo "</pre>";
 ?>
