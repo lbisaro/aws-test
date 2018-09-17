522858509ecab40da14942343480a3ad1b301252
Leonardo Bisaro <lbisaro@tanet.com.ar>
2018-09-17 11:30:10 -0300

    a

--- git-checkout-master.php
+++ git-checkout-master.php
@@ -9,6 +9,7 @@ foreach ($commands as $command)
     $output = array();
     $return_var = '';
     $string = exec($command,$output,$return_var);
+    echo "<style>body {font-family: arial; font-size: 16px;} </style>";
     echo "<h3><code>".$command.'</code></h3>';
     echo "<table>
     <tr>
